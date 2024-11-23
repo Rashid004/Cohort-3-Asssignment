05995b4d8c40c2f944eae44afc9ed4de6ab3850b
Ansari Rashid <rashidansari3868038@gmail.com>
2024-11-23 18:22:37 +0530

    chore: Create Todo Using ExpressJs

--- project-week4/index.js
+++ project-week4/index.js
@@ -1,29 +1,54 @@
 /** @format */
 
-// /** @format */
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
 
-// const express = require("express");
+// use body parser to parse json
+app.use(bodyParser.json());
 
-// const app = express();
+// Create A Todo
+let todos = [];
 
-// // create todo list app
-// let todos = [];
+// fetch all todos
+app.get("/todos", (req, res) => {
+  res.json(todos);
+});
 
-// app.get("/todos", function (req, res) {
-//   res.json(todos);
-// });
+// create a todo & Post a Todo
+app.post("/todos", (req, res) => {
+  const todo = {
+    id: todos.length + 1,
+    title: req.body.title || "",
+    completed: req.body.completed || false,
+  };
+  todos.push(todo);
+  res.status(201).json(todo);
+});
 
-// app.post("/todos", (req, res) => {
-//   const todo = {
-//     id: todos.length + 1,
-//     title: req.body.title,
-//     complete: req.body.complete || false,
-//   };
-//   todos.push(todo);
-//   res.status(201).json(todo);
-// });
+// update a todo
+app.put("/todos/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const todo = todos.find((todo) => todo.id === id);
+  if (!todo) {
+    res.status(404).json({ error: "Todo not found" });
+  }
+  todo.title = req.body.title || todo.title;
+  todo.completed = req.body.completed || todo.completed;
+  res.json(todo);
+});
 
-// app.listen(3000);
+// delete a todo
+app.delete("/todos/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = todos.findIndex((todo) => todo.id === id);
+  if (index === -1) {
+    res.status(404).json({ error: "Todo not found" });
+  }
+  todos.splice(index, 1);
+  res.status(204).send();
+});
 
-const chalk = require("chalk");
-console.log(chalk.red("Hello World"));
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
